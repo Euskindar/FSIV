c503018298992e0c92926c6a0a5c469409e70572
Jolon <julenjph@gmail.com>
2022-09-26 16:11:00 +0200

    A

---
+++ Pr2/fsiv_p2/build/CMakeFiles/pixgui.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/pixgui.dir/common_code.cpp.o"
+  "CMakeFiles/pixgui.dir/common_code.cpp.o.d"
+  "CMakeFiles/pixgui.dir/pixgui.cpp.o"
+  "CMakeFiles/pixgui.dir/pixgui.cpp.o.d"
+  "pixgui"
+  "pixgui.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/pixgui.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
