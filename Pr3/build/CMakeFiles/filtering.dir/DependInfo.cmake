53e7142c03dc44b5edcb22603f5e72d1475f0870
Jolon <julenjph@gmail.com>
2022-10-10 14:20:57 +0200

    CAMBIOS CASA

---
+++ Pr3/build/CMakeFiles/filtering.dir/DependInfo.cmake
@@ -0,0 +1,21 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/jolon/Escritorio/FSIV/Pr3/common_code.cpp" "CMakeFiles/filtering.dir/common_code.cpp.o" "gcc" "CMakeFiles/filtering.dir/common_code.cpp.o.d"
+  "/home/jolon/Escritorio/FSIV/Pr3/convolution.cpp" "CMakeFiles/filtering.dir/convolution.cpp.o" "gcc" "CMakeFiles/filtering.dir/convolution.cpp.o.d"
+  "/home/jolon/Escritorio/FSIV/Pr3/filtering.cpp" "CMakeFiles/filtering.dir/filtering.cpp.o" "gcc" "CMakeFiles/filtering.dir/filtering.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ Pr3/build/CMakeFiles/filtering.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/filtering.dir/common_code.cpp.o"
+  "CMakeFiles/filtering.dir/common_code.cpp.o.d"
+  "CMakeFiles/filtering.dir/convolution.cpp.o"
+  "CMakeFiles/filtering.dir/convolution.cpp.o.d"
+  "CMakeFiles/filtering.dir/filtering.cpp.o"
+  "CMakeFiles/filtering.dir/filtering.cpp.o.d"
+  "filtering"
+  "filtering.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/filtering.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
