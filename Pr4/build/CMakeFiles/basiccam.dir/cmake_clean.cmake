2591b4d1a845441f4a1ba273ca471112ebc3f7e0
Jolon <37414389+Euskindar@users.noreply.github.com>
2022-10-31 09:23:16 +0100

    Add files via upload

---
+++ Pr4/build/CMakeFiles/basiccam.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/basiccam.dir/basiccam.cpp.o"
+  "CMakeFiles/basiccam.dir/basiccam.cpp.o.d"
+  "CMakeFiles/basiccam.dir/common_code.cpp.o"
+  "CMakeFiles/basiccam.dir/common_code.cpp.o.d"
+  "basiccam"
+  "basiccam.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/basiccam.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ Pr4/build/CMakeFiles/basiccam.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for basiccam.
+# This may be replaced when dependencies are built.
